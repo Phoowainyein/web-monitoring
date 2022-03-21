4a77d3c316b800db465c136cdf191897ca331ea8
Phoowainyein <phoo.nyein@student.samk.fi>
2022-03-21 21:52:27 +0200

    edited kafka_website_checker

--- kafka_website_checker.py
+++ kafka_website_checker.py
@@ -10,59 +10,70 @@ import pandas as pd
 from pydoc_data.topics import topics
 from urllib import response
 from kafka import KafkaConsumer,TopicPartition
+import os
+
 
 
 if __name__ == '__main__':
-    lst_to_csv('target_websites.csv',website_lists)#from functionality.py
-    time.sleep(2.5)
-    data=pd.read_csv('target_websites.csv')
+    while True:
+        try:
+            start = time.time()
+            os.remove('target_websites.csv')
+            lst_to_csv('target_websites.csv',website_lists)#from functionality.py
+            time.sleep(2.5)
+            data=pd.read_csv('target_websites.csv')
 
-    
-    for websites in data.target_websites:
+            
+            for websites in data.target_websites:
+                
+                producer = KafkaProducer(
+                bootstrap_servers='kafka-1ac287d-phoowai1995-bc35.aivencloud.com:17205',
+                security_protocol="SSL",
+                ssl_cafile="./ca.pem",
+                ssl_certfile="./service.cert",
+                ssl_keyfile="./service.key",
+                value_serializer= lambda value:json.dumps(value).encode('utf-8')
+                )
+                producer.send('website-checker',monitor_(websites))
+                producer.flush()    
+                            
+            print("Producer  run successfully!") 
+            time.sleep(5)
+            consumer = KafkaConsumer(
+            bootstrap_servers='kafka-1ac287d-phoowai1995-bc35.aivencloud.com:17205',
+            security_protocol="SSL",
+            ssl_cafile="./ca.pem",
+            ssl_certfile="./service.cert",
+            ssl_keyfile="./service.key",
+            value_deserializer = lambda value: json.loads(value.decode('utf-8')),
+            auto_offset_reset='earliest',
+            )
+            consumer.subscribe(topics='website-checker')
+            
+            status_code_= []
+            response_time=[]
+            #I could have use message.offset  or other method to stop the loop ,however I like to keep it simple to break the loop with the number
+            #of rows from our dataframe
+            count=0
+            for message in consumer:
+                count+=1
+                response_time.append(message.value[0])
+                status_code_.append(message.value[1])
+                if count==len(data):
+                    break
+            print(status_code_,response_time)
+            print("Consumer run successfully!")
+
+            #storing the data  consumer has fetched into csv file
+            data['status_code']=status_code_
+            data['response_time']=response_time
+            data.to_csv('target_websites.csv',index=False)
+            print("Successfully store the data into csv !")
+            end = time.time()
+            time.sleep(60)
+        except KeyboardInterrupt:
+            sys.exit("keyboard interrupting !")
         
-        producer = KafkaProducer(
-        bootstrap_servers='kafka-1ac287d-phoowai1995-bc35.aivencloud.com:17205',
-        security_protocol="SSL",
-        ssl_cafile="./ca.pem",
-        ssl_certfile="./service.cert",
-        ssl_keyfile="./service.key",
-        value_serializer= lambda value:json.dumps(value).encode('utf-8')
-        )
-        producer.send('website-checker',monitor_(websites))
-        producer.flush()    
-                      
-    print("Producer  run successfully!") 
-    time.sleep(5)
-    consumer = KafkaConsumer(
-    bootstrap_servers='kafka-1ac287d-phoowai1995-bc35.aivencloud.com:17205',
-    security_protocol="SSL",
-    ssl_cafile="./ca.pem",
-    ssl_certfile="./service.cert",
-    ssl_keyfile="./service.key",
-    value_deserializer = lambda value: json.loads(value.decode('utf-8')),
-    auto_offset_reset='earliest',
-    )
-    consumer.subscribe(topics='website-checker')
-    
-    status_code_= []
-    response_time=[]
-    #I could have use message.offset  or other method to stop the loop ,however I like to keep it simple to break the loop with the number
-    #of rows from our dataframe
-    count=0
-    for message in consumer:
-        count+=1
-        response_time.append(message.value[0])
-        status_code_.append(message.value[1])
-        if count==len(data):
-            break
-    print(status_code_,response_time)
-    print("Consumer run successfully!")
-
-    #storing the data  consumer has fetched into csv file
-    data['status_code']=status_code_
-    data['response_time']=response_time
-    data.to_csv('target_websites.csv',index=False)
-    print("Successfully store the data into csv !")
 
 
 
